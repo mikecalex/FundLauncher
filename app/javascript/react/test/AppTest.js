d83251c1f623b3055ffc972b93353550da5d5af3
mikecalex <mikecalex@gmail.com>
2017-10-30 16:26:28 -0400

    Omniauth

--- app/javascript/react/test/AppTest.js
+++ app/javascript/react/test/AppTest.js
@@ -1,6 +1,6 @@
 import App from '../src/App';
 import React from 'react'
-import { mount } from 'enzyme';
+import { configure } from 'enzyme';
 import jasmineEnzyme from 'jasmine-enzyme';
 
 describe('A test for App', () => {
--- app/javascript/react/test/testHelper.js
+++ app/javascript/react/test/testHelper.js
@@ -3,7 +3,7 @@ import 'babel-polyfill';
 import React from 'react';
 import Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-import { mount } from 'enzyme';
+import { configure } from 'enzyme';
 import jasmineEnzyme from 'jasmine-enzyme';
 
 Enzyme.configure({ adapter: new Adapter() });
