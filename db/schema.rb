ff191e4ef233efcb49a723d3c19cbaaf72b8c012
mikecalex <mikecalex@gmail.com>
2017-10-31 11:46:41 -0400

    Add User Profile Pic

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171030170044) do
+ActiveRecord::Schema.define(version: 20171031153441) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 20171030170044) do
     t.datetime "oauth_expires_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image"
   end
 
 end
